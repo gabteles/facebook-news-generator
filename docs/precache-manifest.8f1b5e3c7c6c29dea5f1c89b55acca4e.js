1926c0e545f6764af139a9b756f8a8d0c881991f
Gabriel Teles <gab.teles@hotmail.com>
2019-04-03 20:19:28 -0300

    adding public folder

---
+++ docs/precache-manifest.8f1b5e3c7c6c29dea5f1c89b55acca4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01374e4076fdc58a4e76",
+    "url": "/facebook-news-generator/static/js/runtime~main.b19b11a1.js"
+  },
+  {
+    "revision": "05a8c9087f65ab5151a6",
+    "url": "/facebook-news-generator/static/js/main.328dca6e.chunk.js"
+  },
+  {
+    "revision": "7c9c12355daf488783a0",
+    "url": "/facebook-news-generator/static/js/2.b3e8c75d.chunk.js"
+  },
+  {
+    "revision": "05a8c9087f65ab5151a6",
+    "url": "/facebook-news-generator/static/css/main.df3b6a9f.chunk.css"
+  },
+  {
+    "revision": "98f41e48f96efd228a38f68c9b2c6c68",
+    "url": "/facebook-news-generator/index.html"
+  }
+];
